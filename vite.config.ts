f13f36afc3e9bd1992e862b7f472139e747e6837
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-05-06 12:37:25 +0600

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
 import { visualizer } from "rollup-plugin-visualizer";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-	plugins: [nodePolyfills(), sveltekit(), visualizer()],
+	plugins: [sveltekit(), visualizer()],
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"]
 	}
