26c5ac6da3841a331a3d7c4e0fd2eadda0647245
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-05-06 12:44:25 +0600

    add

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from "vitest/config";
 import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-	plugins: [sveltekit(), visualizer()],
+	plugins: [
+		sveltekit(),
+		visualizer({
+			emitFile: true,
+			filename: "stats.html"
+		})
+	],
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"]
 	}
