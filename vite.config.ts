3f6605daab3241adb1f54420c5c4f5a05524993a
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-05-06 12:52:54 +0600

    Remove scalar as it's not compatible with node adapter (#23)
    * add
    
    * add
    
    * add
    
    * ad
    
    * add
    
    * add
    
    * Update package.json
    
    * Update package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
 import { visualizer } from "rollup-plugin-visualizer";
+
 export default defineConfig({
-	plugins: [sveltekit(), visualizer()],
+	plugins: [
+		sveltekit(),
+		visualizer({
+			emitFile: true,
+			filename: "stats.html"
+		})
+	],
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"]
 	}
