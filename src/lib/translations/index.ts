c0fbed9b930352986a43107beef353b877288a73
tokitouq <suneeth.since2005@gmail.com>
2024-05-18 23:51:52 +0530

    add basic i18n config

---
+++ src/lib/translations/index.ts
@@ -0,0 +1,19 @@
+import i18n, { type Config } from "sveltekit-i18n";
+import { dev } from "$app/environment";
+import lang from "./lang.json";
+
+export const default_locale = "en";
+
+export const config = {
+	log: {
+		level: dev ? "warn" : "error",
+	},
+	translations: {
+		en: { lang },
+		jp: { lang },
+	}
+} satisfies Config;
+
+export const { t, loading, locales, locale, translations, loadTranslations, addTranslations, setLocale, setRoute } = new i18n(config);
+
+loading.subscribe(($loading) => $loading && console.log('Loading translations...'));
