d82d66c57c4c24835293c95a471801e8df76455c
tokitouq <suneeth.since2005@gmail.com>
2024-05-20 13:24:16 +0530

    style: fix html and add basic snippet

--- src/routes/[...lang=locale]/anime/+page.svelte
+++ src/routes/[...lang=locale]/anime/+page.svelte
@@ -128,14 +128,33 @@
 
 		progress_value = value;
 	});
+
+	const latest_episodes = [
+		{
+			cover: "https://coreproject-v3-ui.vercel.app/images/cover/one_piece.webp",
+			banner: "https://coreproject-v3-ui.vercel.app/images/SpyxFamily.avif",
+			title: "One Piece",
+			ep_number: 7,
+			timestamp: "1 hour ago", // TODO: format time
+		},
+		{
+			cover: "https://coreproject-v3-ui.vercel.app/images/cover/jjk.webp",
+			banner: "https://coreproject-v3-ui.vercel.app/images/AharenSan.avif",
+			title: "Jujutsu Kaisen season 2",
+			ep_number: 2,
+			timestamp: "2 hour ago", // TODO: format time
+		}
+	];
 </script>
 
-<div class="mt-16 block md:mt-0 md:p-[1.25vw] md:pr-[3.75vw]">
-	<div class="flex flex-col justify-between md:flex-row">
-		<!-- LATEST ANIME SECTION-->
+{#snippet latest_episodes_card(episode: typeof latest_episodes[0])}
+	<div class="w-full h-max bg-neutral">{episode.title}</div>
+{/snippet}
 
+<div class="mt-16 block md:mt-0 md:p-[1.25vw] md:pr-[3.75vw]">
+	<div class="grid md:grid-cols-2 md:gap-[3vw]">
 		<div
-			class="relative h-96 w-full md:h-[27.875vw] md:w-[42.1875vw]"
+			class="relative h-96 w-full md:h-[27.875vw]"
 			use:swipe={{ timeframe: 300, minSwipeDistance: 100, touchAction: "pan-y" }}
 			onswipe={swipe_handler}
 			bind:this={main_hero_slider_element}
@@ -168,49 +187,29 @@
 							class="absolute h-full w-full object-cover object-center md:rounded-t-[0.875vw]"
 						/>
 
-						<div
-							class="md:to-surface-900/25 absolute inset-0 bg-gradient-to-t from-secondary/90 to-secondary/50"
-						></div>
-						<div
-							class="from-surface-900 to-surface-900/25 md:from-surface-900/50 absolute inset-0 hidden bg-gradient-to-r md:flex"
-						></div>
+						<div class="md:to-surface-900/25 absolute inset-0 bg-gradient-to-t from-secondary/90 to-secondary/50"></div>
+						<div class="from-surface-900 to-surface-900/25 md:from-surface-900/50 absolute inset-0 hidden bg-gradient-to-r md:flex"></div>
 
-						<div
-							class="absolute bottom-0 flex flex-col p-4 md:left-0 md:px-[3.75vw] md:py-[2.625vw]"
-						>
-							<span class="text-3xl font-bold text-white md:text-[2vw] md:leading-[2.375vw]"
-								>{anime.name}</span
-							>
-							<div
-								class="flex flex-wrap items-center gap-2 py-2 text-xs font-semibold text-white/90 md:gap-[0.65vw] md:pb-0 md:pt-[0.5vw] md:text-[0.9375vw]"
-							>
-								<span class="leading-[1.125vw]">
-									{anime.type}
-								</span>
+						<div class="absolute bottom-0 flex flex-col p-4 md:left-0 md:px-[3.75vw] md:py-[2.625vw]">
+							<span class="text-3xl font-bold text-white md:text-[2vw] md:leading-[2.375vw]">{anime.name}</span>
+							<div class="flex flex-wrap items-center gap-2 py-2 text-xs font-semibold text-white/90 md:gap-[0.65vw] md:pb-0 md:pt-[0.5vw] md:text-[0.9375vw]">
+								<span class="leading-[1.125vw]">{anime.type}</span>
 								<Circle class="w-1 opacity-75 md:w-[0.25vw]" />
-								<span class="leading-[1.125vw]">
-									{anime.episodes} eps
-								</span>
+								<span class="leading-[1.125vw]">{anime.episodes} eps</span>
 								<Circle class="w-1 opacity-75 md:w-[0.25vw]" />
 								<span class="leading-[1.125vw]">Completed</span>
 								<Circle class="w-1 opacity-75 md:w-[0.25vw]" />
-								<span class="capitalize leading-[1.125vw]">
-									{formated_aired_on}
-								</span>
+								<span class="capitalize leading-[1.125vw]">{formated_aired_on}</span>
 								<Circle class="w-1 opacity-75 md:w-[0.25vw]" />
-								<span class="leading-[1.125vw]">
-									{anime.studio}
-								</span>
+								<span class="leading-[1.125vw]">{anime.studio}</span>
 								<div>
-									<anime-genres class="flex gap-2 pb-2 pt-3 md:gap-[0.5vw] md:pt-0">
+									<div class="flex gap-2 pb-2 pt-3 md:gap-[0.5vw] md:pt-0">
 										{#each anime.genres as genre}
-											<span
-												class="rounded-lg bg-secondary p-2 px-3 text-xs capitalize leading-none md:rounded-[0.35vw] md:px-[0.75vw] md:py-[0.4vw] md:text-[0.75vw] md:font-semibold"
-											>
+											<span class="rounded-lg bg-secondary p-2 px-3 text-xs capitalize leading-none md:rounded-[0.35vw] md:px-[0.75vw] md:py-[0.4vw] md:text-[0.75vw] md:font-semibold">
 												{genre}
 											</span>
 										{/each}
-									</anime-genres>
+									</div>
 
 									<ScrollArea
 										gradient_mask
@@ -236,9 +235,7 @@
 											<Info class="text-surface-50 w-5 md:w-[1.25vw]" />
 											<span>Details</span>
 										</a>
-										<button
-											class="bg-surface-900 text-surface-50s btn btn-secondary h-max min-h-max rounded-xl p-4 text-[3vw] font-bold leading-none md:rounded-[0.5vw] md:p-[0.9vw] md:text-[0.875vw]"
-										>
+										<button class="bg-surface-900 text-surface-50s btn btn-secondary h-max min-h-max rounded-xl p-4 text-[3vw] font-bold leading-none md:rounded-[0.5vw] md:p-[0.9vw] md:text-[0.875vw]">
 											<Edit
 												variant="without_underline_around_pencil"
 												class="text-surface-50 w-4 md:w-[1.25vw]"
@@ -254,8 +251,7 @@
 
 			<div class="absolute bottom-0 flex w-full flex-col">
 				<div
-					class="h-[0.2rem] bg-warning md:h-[0.145vw] {slide_buttons[main_hero_slide_active_index]
-						.background}"
+					class="h-[0.2rem] bg-warning md:h-[0.145vw] {slide_buttons[main_hero_slide_active_index].background}"
 					style="width: {$tweened_progress_value}%;"
 				></div>
 
@@ -299,123 +295,33 @@
 				<Chevron class="w-[1.25vw] -rotate-90" />
 			</button>
 		</div>
-
-		<!-- LATEST ANIME SECTION ENDS HERE -->
-		<div class="hidden w-[21.5625vw] md:block">
-			<div class="flex items-center justify-between pr-[0.75vw]">
-				<div class="flex items-center gap-[0.625vw]">
-					<span class="text-[1.25vw] font-bold">{$t("home.latest_episodes.title")}</span>
-					<button
-						class="btn btn-secondary hidden min-h-full rounded-[0.1875vw] p-0 md:flex md:h-[1.5vw] md:w-[1.5vw]"
-					>
-						<Settings class="w-[0.9vw] opacity-75" />
-					</button>
+		<div class="flex flex-col md:gap-[1vw]">
+			<span class="md:text-[1.35vw] text-accent font-bold">{$t("home.latest_episodes.title")}</span>
+			<div class="flex md:gap-[2vw] size-full">
+				<div class="w-full grid grid-cols-2 md:gap-[1.5vw]">
+					{#each latest_episodes as episode}
+						{@render latest_episodes_card(episode)}
+					{/each}
 				</div>
-				<button
-					class="btn btn-neutral min-h-max rounded-[0.3vw] p-0 text-[0.9vw] font-semibold md:h-[2vw] md:px-[0.5vw]"
-				>
-					<span>{$t("home.latest_episodes.cta")}</span>
-					<ArrowUpRight class="w-[1vw]" />
-				</button>
-			</div>
-			<!-- Latest Episodes Start here-->
-
-			<!-- Latest Episodes Eng Here -->
-			<div class="mt-[0.75vw] flex items-center justify-between gap-[2vw] pr-[0.75vw]">
-				<span class="text-[0.75vw] font-semibold md:leading-[1.25vw]">
-					{$t("home.latest_episodes.options.desc")}
-				</span>
-				<button
-					class="btn btn-secondary h-max min-h-full p-0 text-[0.75vw] font-semibold text-warning"
-				>
-					{$t("home.latest_episodes.options.cta")}
-				</button>
-			</div>
-		</div>
-		<div class="relative mt-[2.75vw] hidden h-[24.5vw] w-[16.625vw] md:block">
-			<div class="absolute h-full w-full rounded-[0.875vw] object-cover object-center">
-				<img
-					class="absolute h-full w-full rounded-[0.875vw] object-cover object-center"
-					src="https://i.pinimg.com/originals/b7/25/12/b725125aaebafbcbf2fb3886a55d2d6f.jpg"
-					alt=""
-					loading="lazy"
-				/>
-			</div>
-			<div class="absolute inset-0 bg-gradient-to-t from-secondary from-[1%] to-secondary/25"></div>
-			<div class="absolute inset-0 bg-gradient-to-r from-secondary/50 to-secondary/25"></div>
-			<div class="absolute inset-0 flex flex-col justify-between px-[1.875vw] pt-[2vw] text-accent">
-				<div class="flex flex-col gap-[0.2w]">
-					<span class="text-[1.5vw] font-bold leading-[1vw]">Welcome</span>
-					<span class="text-[0.875vw] font-semibold leading-[2.5vw]">Jump quickly into</span>
-				</div>
-				<div class="mt-[1vw] flex flex-col gap-[0.75vw]">
-					<div class="flex items-center gap-[1vw]">
-						<button class="btn btn-warning h-[2.5vw] min-h-max w-[2.5vw] rounded-[0.375vw] p-0">
-							<Chat class="text-surface-900 w-[1.25vw]" />
-						</button>
-						<span class="text-[1vw] font-bold">Forums</span>
-					</div>
-					<div class="flex items-center gap-[1vw]">
-						<button
-							class="bg-surface-50 btn btn-warning h-[2.5vw] min-h-max w-[2.5vw] rounded-[0.375vw] p-0"
-						>
-							<Recent class="text-surface-900 w-[1.25vw]" />
-						</button>
-						<span class="text-[1vw] font-bold">Last watched anime</span>
-					</div>
-					<div class="flex items-center gap-[1vw]">
-						<button
-							class="bg-surface-50 btn btn-warning h-[2.5vw] min-h-max w-[2.5vw] rounded-[0.375vw] p-0"
-						>
-							<Notification class="text-surface-900 w-[1.25vw]" />
-						</button>
-						<span class="text-[1vw] font-bold">Notifications</span>
-					</div>
-				</div>
-				<div class="mt-[0.4vw]">
-					<span class="text-[0.9vw] font-semibold leading-none">More</span>
-					<div class="mt-[0.75vw] flex gap-[0.9375vw]">
-						<button class="btn btn-accent h-[2.5vw] min-h-max w-[2.5vw] rounded-[0.375vw] p-0">
-							<Language class="text-surface-900 w-[1.25vw]" />
-						</button>
-						<button class="btn btn-accent h-[2.5vw] min-h-max w-[2.5vw] rounded-[0.375vw] p-0">
-							<Preference class="text-surface-900 w-[1.25vw]" />
-						</button>
-						<button class="btn btn-accent h-[2.5vw] min-h-max w-[2.5vw] rounded-[0.375vw] p-0">
-							<Moon class="text-surface-900 w-[1.25vw]" />
-						</button>
-						<button
-							class="bg-surface-50 btn btn-accent h-[2.5vw] min-h-max w-[2.5vw] rounded-[0.375vw] p-0"
-						>
-							<Settings class="text-surface-900 w-[1.25vw]" />
-						</button>
-					</div>
-				</div>
-				<CoreProjectText />
+				<div class="bg-neutral h-full md:w-[5vw]"></div>
 			</div>
 		</div>
 	</div>
 	<div class="flex flex-col p-4 pt-7 md:mb-[1vw] md:mt-[2.1875vw] md:flex md:w-[68vw] md:p-0">
 		<div class="flex items-center gap-[0.625vw]">
 			<span class="text-lg font-bold md:text-[1.25vw]">{$t("home.my_list.title")}</span>
-			<button
-				class="btn btn-secondary hidden min-h-full rounded-[0.1875vw] p-0 md:flex md:h-[1.5vw] md:w-[1.5vw]"
-			>
+			<button class="btn btn-secondary hidden min-h-full rounded-[0.1875vw] p-0 md:flex md:h-[1.5vw] md:w-[1.5vw]">
 				<Settings class="w-[0.9vw] opacity-75" />
 			</button>
 		</div>
 		<div class="flex items-center justify-between">
 			<span class="text-surface-50 text-sm md:text-[1vw] md:font-semibold"> 7 animes </span>
 			<div class="hidden items-center gap-[1vw] md:flex">
-				<button
-					class="bg-surface-400 btn btn-neutral h-[2.25vw] min-h-max gap-[0.625vw] rounded-[0.375vw] p-0 text-[0.875vw] font-semibold md:px-[0.5vw]"
-				>
+				<button class="bg-surface-400 btn btn-neutral h-[2.25vw] min-h-max gap-[0.625vw] rounded-[0.375vw] p-0 text-[0.875vw] font-semibold md:px-[0.5vw]">
 					{$t("home.my_list.options.watching")}
 					<Chevron class="w-[1vw]" />
 				</button>
-				<button
-					class="bg-surface-400 btn btn-neutral h-[2.25vw] min-h-max gap-[0.625vw] rounded-[0.375vw] p-0 text-[0.875vw] font-semibold md:px-[0.5vw]"
-				>
+				<button class="bg-surface-400 btn btn-neutral h-[2.25vw] min-h-max gap-[0.625vw] rounded-[0.375vw] p-0 text-[0.875vw] font-semibold md:px-[0.5vw]">
 					{$t("home.my_list.cta")}
 					<ArrowUpRight class="w-[1vw]" />
 				</button>
