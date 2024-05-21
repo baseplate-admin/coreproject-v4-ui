25addcfe3538046145d8689d65e69dcc1ea80d94
tokitouq <suneeth.since2005@gmail.com>
2024-05-21 18:44:47 +0530

    readd comment

--- src/routes/[...lang=locale]/anime/+page.svelte
+++ src/routes/[...lang=locale]/anime/+page.svelte
@@ -396,6 +396,7 @@
 								<div class="rounded-full bg-neutral md:mr-[0.5vw] md:size-[2.75vw]"></div>
 							</div>
 						{/if}
+						<!-- use Image component for just to get color -->
 						<Image
 							src={episode.cover}
 							class="hidden"
