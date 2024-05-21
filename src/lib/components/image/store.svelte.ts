209124844022b9c8113d4528599f168b2f15cba3
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-05-21 17:10:36 +0600

    Add

--- src/lib/components/image/store.svelte.ts
+++ src/lib/components/image/store.svelte.ts
@@ -1,1 +1,6 @@
-export let color_mapping = $state<Map<String, number[][]>>(new Map<String, number[][]>());
+import { COLORTHIEF_MAX_ENTRIES } from "$constants/lru";
+import QuickLRU from "quick-lru";
+
+export let color_mapping = $state<QuickLRU<String, number[][]>>(
+	new QuickLRU<String, number[][]>({ maxSize: COLORTHIEF_MAX_ENTRIES })
+);
