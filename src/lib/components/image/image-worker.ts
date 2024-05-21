b13f7dac155c1ab8ec3778fa4a9b9f2989fc1035
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-05-21 08:06:40 +0600

    Fix wasm loading

--- src/lib/components/image/image-worker.ts
+++ src/lib/components/image/image-worker.ts
@@ -1,10 +1,9 @@
 import init, { get_color_thief } from "color-thief-wasm-web";
-import wasm from "color-thief-wasm-web/color_thief_wasm_bg.wasm?url";
 
 const COLOR_COUNT = 255; // Our theoritical limit is 256  = (0+255) colors
 
 onmessage = async function (event) {
-	await init(wasm);
+	await init();
 	let color_palette: number[][] = get_color_thief(
 		new Uint8Array(event.data),
 		64 * 64,
