44fdd49e6d46977e99031e3b001854f531a27151
tokitouq <suneeth.since2005@gmail.com>
2024-05-22 00:09:21 +0530

    fix: add more logic shit

--- src/lib/components/image/index.svelte
+++ src/lib/components/image/index.svelte
@@ -54,8 +54,6 @@
 			const ctx = canvas_element.getContext("2d");
 			const rect = canvas_element.getBoundingClientRect();
 
-			console.log(canvas_element);
-
 			canvas_element.width = rect.width;
 			canvas_element.height = rect.height;
 
@@ -63,7 +61,27 @@
 			// if (ctx) ctx.filter = "brightness(25%)";
 
 			if (ctx) {
-				ctx.drawImage(img, 0, 0, rect.width, rect.height);
+				const img_ratio = img.width / img.height;
+				const canvas_ratio = rect.width / rect.height;
+				let draw_width,
+					draw_height,
+					offset_x,
+					offset_y;
+
+				// check if iamge is wider or taller relative to canvas
+				if (img_ratio > canvas_ratio) {
+					draw_height = rect.height;
+					draw_width = img_ratio * draw_height; // get width with relative to height
+					offset_x = (rect.width - draw_width) / 2; // center iamge with remaining space
+					offset_y = 0;
+				} else {
+					draw_width = rect.width;
+					draw_height = draw_width / img_ratio;
+					offset_x = 0;
+					offset_y = (rect.height - draw_height) / 2;
+				}
+
+				ctx.drawImage(img, offset_x, offset_y, draw_width, draw_height);
 			}
 
 			let imageData = ctx?.getImageData(0, 0, rect.width, rect.height);
