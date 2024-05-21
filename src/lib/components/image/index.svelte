ec290b7411116a41cdcfe711b7e73b7f0f84c5be
tokitouq <suneeth.since2005@gmail.com>
2024-05-21 21:41:03 +0530

    revert: optional prop

--- src/lib/components/image/index.svelte
+++ src/lib/components/image/index.svelte
@@ -12,7 +12,7 @@
 		src: string;
 		class?: string;
 		color_palette: number[][];
-		image_loaded: boolean;
+		image_loaded?: boolean;
 	} = $props();
 
 	let canvas_element = $state<HTMLCanvasElement>();
