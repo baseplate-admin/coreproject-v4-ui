7ba43b122ef1962ed223eca3a0fe223d781bb5fb
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-05-21 07:10:27 +0600

    Update image-worker.ts

--- src/lib/components/image/image-worker.ts
+++ src/lib/components/image/image-worker.ts
@@ -1,7 +1,15 @@
 import init, { get_color_thief } from "color-thief-wasm-web";
+import wasm from "color-thief-wasm-web/color_thief_wasm_bg.wasm?url";
+
+const COLOR_COUNT = 255; // Our theoritical limit is 256  = (0+255) colors
 
 onmessage = async function (event) {
-	await init();
-	let color_palette: number[][] = get_color_thief(new Uint8Array(event.data), 64 * 64, 10, 5);
+	await init(wasm);
+	let color_palette: number[][] = get_color_thief(
+		new Uint8Array(event.data),
+		64 * 64,
+		10,
+		COLOR_COUNT
+	);
 	postMessage(color_palette);
 };
