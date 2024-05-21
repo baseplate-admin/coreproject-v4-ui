6cd0e29e2f25b8db626077631d4cef566f889551
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-05-21 06:48:19 +0600

    add

--- src/lib/components/image/image-worker.ts
+++ src/lib/components/image/image-worker.ts
@@ -1,7 +1,8 @@
 import init, { get_color_thief } from "color-thief-wasm-web";
+import wasm from "color-thief-wasm-web/color_thief_wasm_bg.wasm?url";
 
 onmessage = async function (event) {
-	await init();
+	await init(wasm);
 	let color_palette: number[][] = get_color_thief(new Uint8Array(event.data), 64 * 64, 10, 5);
 	postMessage(color_palette);
 };
