4c3c60689db003e6da1bb9b93979cd4b7a9c6020
tokitouq <suneeth.since2005@gmail.com>
2024-05-22 20:11:27 +0530

    refactor: proper types and codes

--- src/lib/types/anime.ts
+++ src/lib/types/anime.ts
@@ -1,4 +1,4 @@
-export type LatestAnimes = {
+export type LatestAnime = {
 	id: number;
 	name: string;
 	type: string;
---
+++ src/lib/types/episode.ts
@@ -0,0 +1,8 @@
+export type LatestEpisode = {
+	id: number;
+	cover: string;
+	banner: string;
+	title: string;
+	ep_number: number;
+	timestamp: string;
+};
