47cfb6cc97f6c64bd6a285ace9cf8d915bfa0aa3
tokitouq <suneeth.since2005@gmail.com>
2024-05-30 16:10:17 +0530

    fix

--- src/routes/anime/+page.svelte
+++ src/routes/anime/+page.svelte
@@ -57,7 +57,7 @@
 				studio: "tokito",
 				genres: ["hentai", "action", "romance", "smooth"],
 				synopsis: `Since the premiere of the anime adaptation of Eiichiro Oda's One Piece manga in 1999, Toei Animation has produced 15 feature films based on the franchise traditionally released during the Japanese school spring break since 2000.[1] Four of the films were originally shown as double features alongside other Toei film productions and thus have a running time below feature length (between 30 and 56 minutes). The first three films were shown at the Toei Anime Fair (東映アニメフェア, Toei Anime Fea) and the eleventh was released as part of Jump Heroes Film. The films generally use original storylines, but some adapt story arcs from the manga directly. With the release of films ten, twelve, thirteen, and fourteen, tie-in story arcs of the TV series were aired concurrently. `,
-				banner: "/images/mock/banner/one_piece.avif",
+				image: "/images/mock/banner/one_piece.avif",
 			},
 			{
 				id: 3,
