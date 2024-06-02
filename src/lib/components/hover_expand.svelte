08d9a35ebf8025c82416fdf2391be302c23b7cdd
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-06-02 19:22:24 +0600

    Port anime page (#160)

---
+++ src/lib/components/hover_expand.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { cn } from "$functions/classnames";
+	import ScrollArea from "$components/scroll_area.svelte";
+	import type { Snippet } from "svelte";
+
+	let {
+		children,
+		class: klass,
+		height = "",
+		duration = "duration-300",
+		line_clamp = "line-clamp-1"
+	}: { children: Snippet } & Partial<{
+		class: string;
+		height: string;
+		duration: string;
+		line_clamp: string;
+	}> = $props();
+</script>
+
+<ScrollArea
+	class={cn(
+		klass,
+		height,
+		duration,
+		"overflow-hidden ease-in-out scrollbar-none hover:overflow-y-scroll"
+	)}
+>
+	<span
+		class={cn(
+			line_clamp,
+			"md:line-clamp-none [&:not(:hover)]:[mask-image:_linear-gradient(90deg,_rgba(7,5,25)_75%,_rgba(0,0,0,0)_100%)]"
+		)}
+	>
+		{@render children()}
+	</span>
+</ScrollArea>
