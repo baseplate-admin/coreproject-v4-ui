632594dc06ef56b5be75f36adbd0ffca6034946a
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-06-03 14:28:54 +0600

    add

--- src/lib/modals/index.svelte
+++ src/lib/modals/index.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-    import { modal_maps } from "$stores/modals.svelte";
+	import { createModalStore } from "$stores/modals.svelte";
+	import SearchModal from "./search_modal.svelte";
 
-    
+	const modal_store = createModalStore();
 
+	$inspect(modal_store.state);
 </script>
 
+{#if modal_store.get_modal_state("search")}
+	gell
+	<SearchModal />
+{/if}
