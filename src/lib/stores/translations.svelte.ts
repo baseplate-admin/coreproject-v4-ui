4eaad743d5e058d86d7f16b3adeb9c5471eea3b2
tokitouq <suneeth.since2005@gmail.com>
2024-06-04 17:29:43 +0530

    add

--- src/lib/stores/translations.svelte.ts
+++ src/lib/stores/translations.svelte.ts
@@ -1,15 +1,37 @@
+import { browser } from "$app/environment";
+import { default_locale, locales } from "$lib/translations";
 import lang from "$lib/translations/lang.json";
 
-export type ILang = keyof typeof lang;
-let state = $state<ILang>("en");
+type ILang = keyof typeof lang;
+let state = $state<ILang>(default_locale);
+const supported_locales = locales.get().map((l) => l.toLowerCase());
+
+if (browser) {
+	if (localStorage.getItem("lang")) {
+		state = (localStorage.getItem("lang") || "").toLowerCase() as ILang;
+	} else {
+		localStorage.setItem("lang", state);
+	}
+}
 
 export function createLocaleStore() {
 	return {
 		get state() {
 			return state;
 		},
-		set_locale(locale: ILang) {
-			state = locale;
+		set_locale(locale: string) {
+			if (supported_locales.includes(locale)) {
+				state = locale as ILang;
+			}
+			if (browser) {
+				if (!supported_locales.includes(state)) {
+					state = ((navigator.language?.match(/[a-zA-Z]+?(?=-|_|,|;)/)?.[0].toLowerCase() ?? default_locale) as ILang);
+				}
+				// save locale to localstorage
+				if (state !== localStorage.getItem("lang")) {
+					localStorage.setItem("lang", state);
+				}
+			}
 		}
 	};
 };
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,19 +1,14 @@
 import {
-	default_locale,
 	loadTranslations,
-	locales,
 	setLocale,
-	setRoute,
-	translations
+	setRoute
 } from "$lib/translations";
-import { browser } from "$app/environment";
 import { dev } from "$app/environment";
 import type { Load } from "@sveltejs/kit";
-import { createLocaleStore, type ILang } from "$stores/translations.svelte";
+import { createLocaleStore } from "$stores/translations.svelte";
 
 export const load: Load = async ({ url }) => {
 	const locale_store = createLocaleStore();
-	console.log(locale_store.state);
 	// Vercel Shits. Remove when we do master production
 
 	if (!dev) {
@@ -26,23 +21,10 @@ export const load: Load = async ({ url }) => {
 	const { pathname, searchParams } = url;
 
 	if (searchParams && searchParams.has("lang")) {
-		locale_store.set_locale((searchParams.get("lang") || "").toLowerCase() as ILang)
-	} else if (typeof localStorage !== "undefined" && localStorage.getItem("lang")) {
-		locale_store.set_locale((localStorage.getItem("lang") || "").toLowerCase() as ILang);
-	}
+		locale_store.set_locale((searchParams.get("lang") || "").toLowerCase())
+	};
 
-	const supported_locales = locales.get().map((l) => l.toLowerCase());
-
-	// use default locale if current locale is not supported
-	if (browser) {
-		if (!supported_locales.includes(locale_store.state)) {
-			locale_store.set_locale((navigator.language?.match(/[a-zA-Z]+?(?=-|_|,|;)/)?.[0].toLowerCase() ?? default_locale) as ILang);
-		}
-		// save current locale on localStorage
-		if (locale_store.state !== localStorage.getItem("lang")) {
-			localStorage.setItem("lang", locale_store.state);
-		}
-	}
+	console.log(locale_store.state);
 
 	await setRoute(pathname);
 	await setLocale(locale_store.state);
