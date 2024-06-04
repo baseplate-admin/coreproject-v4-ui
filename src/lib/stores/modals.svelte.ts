e65062d82cdfee44bb7eb8e0a7ae41cce660709c
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-06-04 08:30:54 +0600

    add

--- src/lib/stores/modals.svelte.ts
+++ src/lib/stores/modals.svelte.ts
@@ -1,20 +1,21 @@
-import { get, writable } from "svelte/store";
-
 const modals = ["search"] as const;
 type IModals = (typeof modals)[number];
 
-export let modal_store = writable(Object.fromEntries(modals.map((item) => [item, false])));
+export function createModalStore() {
+	let state = $state(Object.fromEntries(modals.map((item) => [item, false])));
 
-export function open_modal(name: IModals) {
-	const obj = get(modal_store);
-	// Close all other modals
-	Object.keys(obj).forEach((v) => (obj[v] = false));
-	obj[name] = true;
-	modal_store.set(obj);
-}
-
-export function close_modal(name: IModals) {
-	const obj = get(modal_store);
-	obj[name] = false;
-	modal_store.set(obj);
+	return {
+		get state() {
+			return state as Record<IModals, boolean>;
+		},
+		open_modal(name: IModals) {
+			// Close all other modals
+			Object.keys(state).forEach((v) => (state[v] = false));
+			state[name] = true;
+			console.log(state);
+		},
+		close_modal(name: IModals) {
+			state[name] = false;
+		}
+	};
 }
