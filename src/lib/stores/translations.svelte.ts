b86e73f929d1f57995be58c3cfa9e15aefe04380
moonlitgrace <tokitousan2005@gmail.com>
2024-06-04 18:39:57 +0530

    refactor: translation store (#177)
    Co-authored-by: baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>

---
+++ src/lib/stores/translations.svelte.ts
@@ -0,0 +1,37 @@
+import { browser } from "$app/environment";
+import { default_locale, locales } from "$lib/translations";
+import lang from "$lib/translations/lang.json";
+
+type ILang = keyof typeof lang;
+
+const supported_locales = locales.get().map((l) => l.toLowerCase());
+
+let state = $state(detect_language() satisfies ILang);
+
+function detect_language() {
+	if (!browser) return default_locale;
+
+	const localstorage_value = localStorage.getItem("lang");
+	if (localstorage_value) return localstorage_value as ILang;
+
+	const browser_lang = navigator.language.split("-")[0];
+	if (browser_lang) return browser_lang as ILang;
+
+	return default_locale;
+}
+
+export function createLocaleStore() {
+	return {
+		get state() {
+			return state;
+		},
+		set_locale(locale: string) {
+			if (supported_locales.includes(locale)) {
+				state = locale as ILang;
+				localStorage.setItem("lang", state);
+			} else {
+				throw new Error(`${locale} is not part of ${supported_locales}`);
+			}
+		}
+	};
+}
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,18 +1,13 @@
-import {
-	default_locale,
-	loadTranslations,
-	locales,
-	setLocale,
-	setRoute,
-	translations
-} from "$lib/translations";
-import { browser } from "$app/environment";
+import { loadTranslations, setLocale, setRoute } from "$lib/translations";
 import { dev } from "$app/environment";
 import type { Load } from "@sveltejs/kit";
+import { createLocaleStore } from "$stores/translations.svelte";
 
 export const load: Load = async ({ url }) => {
-	// Vercel Shits. Remove when we do master production
+	const { pathname, searchParams } = url;
+	const locale_store = createLocaleStore();
 
+	// Vercel Shits. Remove when we do master production
 	if (!dev) {
 		const { injectSpeedInsights } = await import("@vercel/speed-insights/sveltekit");
 		const { inject } = await import("@vercel/analytics");
@@ -20,30 +15,13 @@ export const load: Load = async ({ url }) => {
 		inject({ mode: "production" });
 	}
 
-	const { pathname, searchParams } = url;
-	let locale = "";
-
 	if (searchParams && searchParams.has("lang")) {
-		locale = (searchParams.get("lang") || "").toLowerCase();
-	} else if (typeof localStorage !== "undefined" && localStorage.getItem("lang")) {
-		locale = (localStorage.getItem("lang") || "").toLowerCase();
-	}
-
-	const supported_locales = locales.get().map((l) => l.toLowerCase());
-
-	// use default locale if current locale is not supported
-	if (browser) {
-		if (!supported_locales.includes(locale)) {
-			locale =
-				navigator.language?.match(/[a-zA-Z]+?(?=-|_|,|;)/)?.[0].toLowerCase() || default_locale;
-		}
-		// save current locale on localStorage
-		localStorage.setItem("lang", locale);
+		locale_store.set_locale((searchParams.get("lang") || "").toLowerCase());
 	}
 
 	await setRoute(pathname);
-	await setLocale(locale);
+	await setLocale(locale_store.state);
 	// load translations before return
-	await loadTranslations(locale, pathname);
-	return { pathname, locale };
+	await loadTranslations(locale_store.state, pathname);
+	return { pathname, locale_store };
 };
