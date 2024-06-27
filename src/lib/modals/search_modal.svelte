4d5a994f2953408bf633d39c47f12ba540ffae40
tokitouq <suneeth.since2005@gmail.com>
2024-06-27 11:44:20 +0530

    style: re-design search modal

--- src/lib/modals/search_modal.svelte
+++ src/lib/modals/search_modal.svelte
@@ -96,13 +96,9 @@
 
 <svelte:window onkeyup={(event) => dialog_element?.open && handle_global_input(event)} />
 
-<dialog
-	class="modal bg-secondary/50 backdrop-blur"
-	bind:this={dialog_element}
-	onclose={handle_close}
->
+<dialog class="modal bg-secondary/90" bind:this={dialog_element} onclose={handle_close}>
 	<div
-		class="modal-box flex max-h-max max-w-max scale-[98%] flex-col items-center border-neutral bg-secondary md:rounded-[1vw] md:border-[0.1vw] md:px-[2vw] md:py-[1vw]"
+		class="modal-box flex max-h-max max-w-max scale-[98%] flex-col items-center bg-transparent md:gap-[2vw]"
 	>
 		<form class="relative flex h-[3.5vw] w-[40vw] items-center">
 			<button
@@ -118,7 +114,7 @@
 				onkeypress={handle_search_key_press}
 				type="text"
 				placeholder="Search for animes, mangas and musics"
-				class="size-full border-none bg-neutral font-semibold text-white outline-none !ring-0 placeholder:font-medium placeholder:text-accent/75 md:rounded-[0.625vw] md:p-[1vw] md:pl-[3.50vw] md:text-[1.1vw]"
+				class="size-full border-none bg-neutral font-semibold text-accent outline-none !ring-0 placeholder:font-medium placeholder:text-accent/75 md:rounded-[1vw] md:p-[1vw] md:pl-[3.50vw] md:text-[1.1vw]"
 			/>
 
 			<button
@@ -131,142 +127,49 @@
 			</button>
 		</form>
 
-		<div class="mt-[1.5vw] flex gap-[4vw]">
-			<div>
+		<div class="flex grid grid-cols-3 gap-[4vw] md:h-[30vw] md:w-[70vw]">
+			<div class="flex flex-col md:gap-[0.5vw]">
 				<span class="text-[1.2vw] font-semibold text-info">anime</span>
-				<ScrollArea
-					offset_scrollbar
-					gradient_mask
-					parent_class="md:mt-[0.2vw] md:h-[30vw] md:w-[20vw]"
-					class="size-full"
+				<div
+					class="flex h-full flex-col items-center justify-center gap-[0.2vw] bg-neutral text-[1.1vw] text-accent md:rounded-[1vw]"
 				>
-					<!-- {#if search_promise}
-						{#await search_promise}
-							<div class="grid h-full place-items-center">
-								<span class="loading loading-ring loading-lg"></span>
-							</div>
-						{:then results}
-							{#if !_.isEmpty(results)}
-								<div bind:this={anime_search_results_container_element}>
-									{#each results as item, index}
-										{@const is_active = active_core === "anime" && active_index === index}
-										{@const mapping = [
-											{
-												value: `TV`,
-												class: `flex items-center gap-[0.3vw]`
-											},
-											{
-												value: item.episode_count ? `${item.episode_count} eps` : null,
-												class: "flex items-center gap-[0.3vw]"
-											},
-											{
-												value: item.aired_from
-													? new FormatDate(item.aired_from).format_to_human_readable_form
-													: null,
-												class: "flex items-center gap-[0.3vw]"
-											}
-										]}
-										<a
-											use:enhance_anchor={{ verb: "GET", target: "#page" }}
-											on:mouseenter={() => handle_core_mouse_enter("anime", index)}
-											on:click={() => dialog_element?.close()}
-											href={reverse("anime_info_view", "mal", String(item.mal_id))}
-											class={cn(
-												is_active && "bg-neutral/50",
-												"flex w-full auto-rows-max items-center gap-[1vw] rounded-[0.7vw] p-[0.8vw] transition duration-200 hover:bg-neutral/75"
-											)}
-										>
-											<img
-												src={item.cover}
-												alt={search_query}
-												class="size-[3.5vw] flex-shrink-0 rounded-[0.5vw] object-cover"
-											/>
-											<div class="flex flex-col leading-none md:gap-[0.5vw]">
-												<span
-													class="line-clamp-1 text-[1.1vw] font-semibold leading-none text-white"
-												>
-													{item.name}
-													<span class="font-medium text-accent/50">({item.name_japanese})</span>
-												</span>
-												<div class="flex items-center md:gap-[0.35vw] md:text-[0.9vw]">
-													{#each mapping as item, index}
-														{@const show_dot = index !== mapping.length - 1}
-
-														{#if !_.isNull(item.value)}
-															<span class={item.class}>{item.value}</span>
-															{#if show_dot}
-																<Circle class="md:w-[0.25vw]" />
-															{/if}
-														{/if}
-													{/each}
-												</div>
-											</div>
-										</a>
-									{/each}
-								</div>
-							{:else}
-								<div
-									class="flex h-full flex-col items-center justify-center gap-[0.2vw] text-[1.1vw]"
-								>
-									<span class="font-medium leading-none">No match found!</span>
-									<span class="text-center font-semibold leading-none text-error"
-										>Couldn't find animes with: "{search_query}"</span
-									>
-								</div>
-							{/if}
-						{:catch error}
-							<div
-								class="flex h-full flex-col items-center justify-center gap-[0.2vw] text-[1.1vw]"
-							>
-								<span class="font-medium leading-none">Oh no, something is wrong!</span>
-								<span class="text-center font-semibold leading-none text-error">{@html error}</span>
-							</div>
-						{/await}
-					{:else}
-						<div class="flex h-full flex-col items-center justify-center gap-[0.2vw] text-[1.1vw]">
-							<span class="font-medium leading-none">Search Away</span>
-							<span class="font-semibold leading-none">"Find Your Anime Bliss"</span>
-						</div>
-					{/if} -->
-					<div
-						class="flex h-full flex-col items-center justify-center gap-[0.2vw] text-[1.1vw] text-accent"
-					>
-						<span class="font-medium leading-none text-warning">Search Away</span>
-						<span class="font-semibold leading-none">"Find Your Anime Bliss"</span>
-					</div>
-				</ScrollArea>
-			</div>
-
-			<div>
-				<span class="text-[1.2vw] font-semibold">manga</span>
-				<div class="bg-surface-400 mt-[0.2vw] h-[30vw] w-[21.875vw] rounded-[0.75vw]">
-					<div class="flex h-full flex-col items-center justify-center gap-[0.2vw] text-[1.1vw]">
-						<span class="font-medium leading-none">mangacore integration</span>
-						<span class="font-semibold leading-none">coming soon</span>
-					</div>
+					<span class="font-medium leading-none text-warning">Search Away</span>
+					<span class="font-semibold leading-none">"Find Your Anime Bliss"</span>
 				</div>
 			</div>
-
-			<div>
-				<span class="text-[1.2vw] font-semibold">music</span>
-				<div class="bg-surface-400 mt-[0.2vw] h-[30vw] w-[21.875vw] rounded-[0.75vw]">
-					<div class="flex h-full flex-col items-center justify-center gap-[0.2vw] text-[1.1vw]">
-						<span class="font-medium leading-none">soundcore integration</span>
-						<span class="font-semibold leading-none">coming soon</span>
-					</div>
+			<div class="flex flex-col md:gap-[0.5vw]">
+				<span class="text-[1.2vw] font-semibold">manga</span>
+				<div
+					class="flex h-full flex-col items-center justify-center gap-[0.2vw] bg-neutral text-[1.1vw] text-accent md:rounded-[1vw]"
+				>
+					<span class="font-medium leading-none">mangacore integration</span>
+					<span class="font-semibold leading-none">coming soon</span>
+				</div>
+			</div>
+			<div class="flex flex-col md:gap-[0.5vw]">
+				<span class="text-[1.2vw] font-semibold">sound</span>
+				<div
+					class="flex h-full flex-col items-center justify-center gap-[0.2vw] bg-neutral text-[1.1vw] text-accent md:rounded-[1vw]"
+				>
+					<span class="font-medium leading-none">soundcore integration</span>
+					<span class="font-semibold leading-none">coming soon</span>
 				</div>
 			</div>
 		</div>
 
-		<p class="mt-[2vw] text-[0.8vw]">
-			<b>Note:</b>
-			Navigate through animes, mangas and sounds with
-			<kbd class="kbd kbd-xs rounded-full text-[0.8vw]">▲</kbd>
-			<kbd class="kbd kbd-xs rounded-full text-[0.8vw]">▼</kbd>
-			and
-			<kbd class="kbd kbd-xs rounded-full text-[0.8vw]">Tab</kbd>
-			keys.
-		</p>
+		<div class="flex items-center bg-neutral md:gap-[1vw] md:rounded-[1vw] md:p-[1vw]">
+			<div>
+				<div class="rounded-full bg-success md:size-[0.5vw]"></div>
+				<div class="rounded-full bg-error md:ml-[0.5vw] md:size-[1vw]"></div>
+			</div>
+			<p class="font-semibold text-accent md:text-[1vw]">
+				Want even advanced searching with genres, time-ranges, tags and more? Try the <a
+					href="/anime/explore"
+					class="text-info underline">Explore</a
+				> page
+			</p>
+			<div class="transform rounded-full bg-success md:size-[1.25vw] md:-translate-y-[0.5vw]"></div>
+		</div>
 	</div>
 	<form method="dialog" class="modal-backdrop">
 		<button>close</button>
