5c6bdd0f3f4d8040be7e5af9fa88957d46f58f3d
tokitouq <suneeth.since2005@gmail.com>
2024-06-27 12:18:41 +0530

    feat: add search items

--- src/lib/modals/search_modal.svelte
+++ src/lib/modals/search_modal.svelte
@@ -1,90 +1,54 @@
 <script lang="ts">
-	import { FormatDate } from "$functions/format_date";
 	import { createModalStore } from "$stores/modals.svelte";
 
 	// Icon imports
 
-	import { FETCH_TIMEOUT } from "$constants/fetch";
-	import { cn } from "$functions/classnames";
 	import Search from "$icons/shapes/search.svelte";
 	import Cross from "$icons/shapes/cross.svelte";
-	import ScrollArea from "$components/scroll_area.svelte";
+	import Circle from "$icons/shapes/circle.svelte";
+	import Chevron from "$icons/shapes/chevron.svelte";
 
 	const modal_store = createModalStore();
 
-	let active_index = $state(0),
-		active_core = $state<"anime" | "manga" | "sound">("anime"),
-		search_query = $state("");
+	let search_query = $state("");
 
 	// Bindings
-	let dialog_element: HTMLDialogElement | null = null,
-		anime_search_results_container_element: HTMLDivElement | null = null;
+	let dialog_element: HTMLDialogElement | null = null;
 
-	const handle_search_key_down = async (e: KeyboardEvent) => {
+	const handle_search_key_down = async () => {
 			if (!dialog_element?.open) return;
-		},
-		handle_search_key_press = async (e: KeyboardEvent) => {
-			if (e.key.toLowerCase() === "enter") return;
-			active_index = 0;
-		},
-		handle_core_mouse_enter = async (core: typeof active_core, index: number) => {
-			active_index = index;
-			active_core = core;
 		},
 		handle_input = async () => {
 			// search_promise = search_query === "" ? null : get_anime_with_serach_parameters();
 		},
 		handle_close = async () => {
 			modal_store.close_modal("search");
-		},
-		handle_global_input = async (e: KeyboardEvent) => {
-			// const search_results = await search_promise;
-			// if (!search_results) return;
-			// switch (e.key.toLowerCase()) {
-			// 	case "arrowdown":
-			// 		active_index = (active_index + 1) % search_results.length;
-			// 		break;
-			// 	case "arrowup":
-			// 		active_index = (active_index - 1 + search_results.length) % search_results.length;
-			// 		break;
-			// 	// do tab logic of switching core
-			// 	// case "tab":
-			// 	//     break;
-			// 	case "enter":
-			// 		const active_anime_element = anime_search_results_container_element?.children[
-			// 			active_index
-			// 		] as HTMLAnchorElement;
-			// 		active_anime_element.click();
-			// 		break;
-			// 	default:
-			// 		break;
-			// }
 		};
 
-	const get_anime_with_serach_parameters = async () => {
-		// const headers: { [key: string]: string } = {};
-		// if ($user_authenticated) {
-		// 	headers["X-CSRFToken"] = get_csrf_token();
-		// }
-		// const res = await fetch(
-		// 	reverse(`anime-list`) + "?" + new URLSearchParams({ name: search_query }),
-		// 	{
-		// 		method: "GET",
-		// 		headers: {
-		// 			Accept: "application/json",
-		// 			"Content-Type": "application/json",
-		// 			"X-CSRFToken": get_csrf_token()
-		// 		},
-		// 		signal: AbortSignal.timeout(FETCH_TIMEOUT)
-		// 	}
-		// );
-		// const json = await res.json();
-		// if (res.ok) {
-		// 	return json["results"] as Array<Anime>;
-		// } else {
-		// 	throw new Error("Something is wrong from the backend");
-		// }
-	};
+	// const get_anime_with_serach_parameters = async () => {
+	// const headers: { [key: string]: string } = {};
+	// if ($user_authenticated) {
+	// 	headers["X-CSRFToken"] = get_csrf_token();
+	// }
+	// const res = await fetch(
+	// 	reverse(`anime-list`) + "?" + new URLSearchParams({ name: search_query }),
+	// 	{
+	// 		method: "GET",
+	// 		headers: {
+	// 			Accept: "application/json",
+	// 			"Content-Type": "application/json",
+	// 			"X-CSRFToken": get_csrf_token()
+	// 		},
+	// 		signal: AbortSignal.timeout(FETCH_TIMEOUT)
+	// 	}
+	// );
+	// const json = await res.json();
+	// if (res.ok) {
+	// 	return json["results"] as Array<Anime>;
+	// } else {
+	// 	throw new Error("Something is wrong from the backend");
+	// }
+	// };
 	// let search_promise: Promise<Anime[]> | null = null;
 
 	$effect(() => {
@@ -94,7 +58,7 @@
 	});
 </script>
 
-<svelte:window onkeyup={(event) => dialog_element?.open && handle_global_input(event)} />
+<svelte:window onkeyup={() => dialog_element?.open} />
 
 <dialog class="modal bg-secondary/90" bind:this={dialog_element} onclose={handle_close}>
 	<div
@@ -111,7 +75,6 @@
 				bind:value={search_query}
 				oninput={handle_input}
 				onkeydown={handle_search_key_down}
-				onkeypress={handle_search_key_press}
 				type="text"
 				placeholder="Search for animes, mangas and musics"
 				class="size-full border-none bg-neutral font-semibold text-accent outline-none !ring-0 placeholder:font-medium placeholder:text-accent/75 md:rounded-[1vw] md:p-[1vw] md:pl-[3.50vw] md:text-[1.1vw]"
@@ -130,11 +93,36 @@
 		<div class="flex grid grid-cols-3 gap-[4vw] md:h-[30vw] md:w-[70vw]">
 			<div class="flex flex-col md:gap-[0.5vw]">
 				<span class="text-[1.2vw] font-semibold text-info">anime</span>
-				<div
-					class="flex h-full flex-col items-center justify-center gap-[0.2vw] bg-neutral text-[1.1vw] text-accent md:rounded-[1vw]"
-				>
-					<span class="font-medium leading-none text-warning">Search Away</span>
-					<span class="font-semibold leading-none">"Find Your Anime Bliss"</span>
+				<div class="flex h-full flex-col bg-neutral md:rounded-[1vw] md:p-[1vw]">
+					<div class="flex flex-col md:gap-[1vw]">
+						{#each { length: 2 } as _}
+							<a href="/anime/mal/1" class="group flex items-center md:gap-[1vw]">
+								<img
+									src="/images/mock/cover/one_piece.webp"
+									alt=""
+									class="object-cover md:size-[4vw] md:rounded-[1vw]"
+								/>
+								<div class="flex flex-col md:gap-[0.2vw]">
+									<h3 class="font-bold text-accent group-hover:text-info md:text-[1vw]">
+										One Piece
+									</h3>
+									<div class="flex items-center font-medium md:gap-[0.5vw] md:text-[0.8vw]">
+										<span>1996</span>
+										<span>TV</span>
+										<Circle class="md:size-[0.25vw]" />
+										<span>1009 eps</span>
+									</div>
+								</div>
+							</a>
+						{/each}
+					</div>
+					<a
+						href="/anime/explore?name=one-piece"
+						class="mt-auto flex items-center self-center text-info md:gap-[0.5vw]"
+					>
+						<span class="font-semibold md:text-[1vw]">view all</span>
+						<Chevron class="-rotate-90 md:size-[1vw]" />
+					</a>
 				</div>
 			</div>
 			<div class="flex flex-col md:gap-[0.5vw]">
