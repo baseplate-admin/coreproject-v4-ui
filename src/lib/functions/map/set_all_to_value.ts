a3e8116899e0f1f921ac3b7f68755b274f6d59ae
meowsocute <meowsocute111@gmail.com>
2024-07-13 18:49:15 +0530

    🔄 Updated `.gitignore`. 🛠 Created fallback component for upcoming pages in `$components/dev.todo.svelte.` 🔄 Replaced deprecated `Map` with new `SvelteMap`. 🐛 Fixed minor bugs in `/anime/+page.svelte` (Updated href URLs).

--- src/lib/functions/map/set_all_to_value.ts
+++ src/lib/functions/map/set_all_to_value.ts
@@ -1,4 +1,4 @@
-export const set_all_to_value = <K, V>(map: Map<K, V>, value: V) => {
+export const set_all_to_value = <K, V>(map: SvelteMap<K, V>, value: V) => {
 	[...map.keys()].forEach((key) => {
 		map.set(key, value);
 	});
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import DevTodo from "$components/dev.todo.svelte";
+</script>
+
+<DevTodo />
