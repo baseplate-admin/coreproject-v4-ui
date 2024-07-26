2d3366aedf40724e5aa48eb90b2dda0d2936aa7b
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-07-26 21:27:14 +0600

    better

--- src/lib/functions/urls/url_matches_with_pathname.ts
+++ src/lib/functions/urls/url_matches_with_pathname.ts
@@ -4,12 +4,4 @@ export const url_matches_with_pathname = ({
 }: {
 	url: string | undefined;
 	pathname: string;
-}): boolean => {
-	if (url === undefined) return false;
-
-	const regex = new RegExp(`${url}\/?$`);
-	if (regex.test(pathname)) {
-		return true;
-	}
-	return false;
-};
+}): boolean => url !== undefined && new RegExp(`${url}/?$`).test(pathname);
