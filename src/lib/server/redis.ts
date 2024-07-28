1b44b86aaa35c7dd4d338799e5df1e5b6c3a84af
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-07-28 13:49:31 +0600

    FINALLY FIX REDIS
    Update redis.ts
    
    Update node.js.yml
    
    Update redis.ts
    
    Update node.js.yml
    
    Update node.js.yml
    
    Update node.js.yml
    
    Update node.js.yml
    
    Update node.js.yml
    
    Update node.js.yml
    
    Update node.js.yml
    
    Update node.js.yml
    
    Update node.js.yml
    
    Update node.js.yml
    
    Update redis.ts
    
    Update node.js.yml
    
    Update node.js.yml
    
    Update redis.ts
    
    Update redis.ts
    
    Update node.js.yml
    
    Update node.js.yml
    
    Update redis.ts

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -1,12 +1,11 @@
 import { createClient } from "redis";
 import { env } from "$env/dynamic/private";
 
-export const redis_client = createClient({
-	url: env.REDIS_URL
-});
-
-redis_client.on("error", function (err) {
-	throw new Error(err);
-});
-
-await redis_client.connect();
+export const redis_client = await createClient({
+	url: env.REDIS_URL ?? undefined,
+	socket: {
+		tls: env.REDIS_TLS === "true"
+	}
+})
+	.on("error", (err) => console.log(err))
+	.connect();
