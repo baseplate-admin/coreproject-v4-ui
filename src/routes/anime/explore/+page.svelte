eff724b7f54c0a357226b2bf2c4763cc4b0cc4f5
meowsocute <meowsocute111@gmail.com>
2024-08-01 17:39:54 +0530

    Ported /explore page

--- src/routes/anime/explore/+page.svelte
+++ src/routes/anime/explore/+page.svelte
@@ -1,13 +1,36 @@
 <script lang="ts">
+	// import * as _ from "lodash-es";
+	import * as _ from "lodash-es"
+	import HoverExpand from "$components/hover_expand.svelte";
 	import ScrollArea from "$components/scroll_area.svelte";
 	import { cn } from "$functions/classnames";
+	import { FormatDate } from "$functions/format_date";
 	import Chevron from "$icons/shapes/chevron.svelte";
+	import Circle from "$icons/shapes/circle.svelte";
 	import Cross from "$icons/shapes/cross.svelte";
 	import MoreBox from "$icons/shapes/more_box.svelte";
 	import Search from "$icons/shapes/search.svelte";
+	import SixGrids from "$icons/shapes/six_grids.svelte";
 	import Tick from "$icons/shapes/tick.svelte";
+	import Expand from "$icons/shapes/expand.svelte";
+	import { type Anime } from "$types/anime";
+	import { FETCH_TIMEOUT } from "$constants/fetch";
+	import { reverse } from "$functions/urls/reverse";
+	import { get_csrf_token } from "$functions/get_csrf_token";
 
-	type FilterOption = {
+	// Binding
+	let search_query = "",
+		active_filters: Array<string> = [],
+		thumbnail_mode: "card_with_dropdown" | "detailed_card" = "card_with_dropdown";
+
+	let search_promise: Promise<Anime[]> | null = null;
+
+	// onMount(async () => {
+	// 	search_promise = get_anime_with_serach_parameters();
+	// });
+
+	// Mapping
+	let filter_options_mapping: {
 		[key: string]: {
 			title: string;
 			class: string;
@@ -15,12 +38,7 @@
 			items: Array<string | number>;
 			selected_items: string[] | null;
 		};
-	};
-
-	let active_filters: Array<string> = [],
-		search_query: string = "";
-
-	let filter_options_mapping: FilterOption = {
+	} = {
 		time_range: {
 			title: "Time Range",
 			class: "hidden flex-col md:gap-[0.35vw]",
@@ -37,7 +55,7 @@
 		},
 		year: {
 			title: "Year",
-			class: "md:flex flex-col md:gap-[0.35vw]",
+			class: "hidden md:flex flex-col md:gap-[0.35vw]",
 			value: "",
 			items: [2020, 2021, 2022, 2023],
 			selected_items: []
@@ -72,14 +90,9 @@
 		}
 	};
 
-	const handle_input = async () => {},
-		isEmpty = (list: any[]) => {
-			if (list.length == 0) return true;
-			else return false;
-		},
-		clear_selected_items = (key: string) => {
-			filter_options_mapping[key].selected_items = [];
-			handle_input();
+	// Functions
+	const handle_input = async () => {
+			search_promise = get_anime_with_serach_parameters();
 		},
 		update_selected_items = (key: string, selected_item: string) => {
 			let filter_option = filter_options_mapping[key];
@@ -101,60 +114,131 @@
 
 			// update filer_options_mapping
 			filter_options_mapping[key] = filter_option;
+			// run fetch
 			handle_input();
+		},
+		clear_selected_items = (key: string) => {
+			filter_options_mapping[key].selected_items = [];
+			// run fetch
+			handle_input();
+		},
+		change_thumbnail_mode = (mode: typeof thumbnail_mode) => {
+			thumbnail_mode = mode;
 		};
+		// isEmpty = (list: any[]) => {
+		// 	if (list.length == 0) return true;
+		// 	else return false;
+		// };
+
+	const get_anime_with_serach_parameters = async (): Promise<Anime[]> => {
+		let url = new URL(
+			window.location.protocol +
+				"//" +
+				window.location.hostname +
+				":" +
+				window.location.port +
+				reverse(`anime-list`)
+		);
+
+		const search_map = {
+			name: search_query,
+			genres: filter_options_mapping["genres"].selected_items
+				?.map((item) => {
+					return item.toLowerCase();
+				})
+				.join()
+		};
+		for (const [key, val] of Object.entries(search_map)) {
+			if (!_.isEmpty(val) && !_.isUndefined(val)) {
+				url.searchParams.set(key, val);
+			}
+		}
+
+		const res = await fetch(url, {
+			method: "GET",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+				"X-CSRFToken": get_csrf_token()
+			},
+			signal: AbortSignal.timeout(FETCH_TIMEOUT)
+		});
+		const json = await res.json();
+
+		if (res.ok) {
+			return _.uniqBy(json["results"], "mal_id") as Array<Anime>;
+		} else {
+			throw new Error("Something is wrong from the backend");
+		}
+	};
 </script>
 
-<section class="mt-20 select-none px-5 text-white md:mt-0">
-	<div class="flex flex-col">
+<section
+	class="mt-20 flex flex-col p-5 md:mt-0 md:gap-[1.5vw] md:pb-[2.5vw] md:pl-[1.5vw] md:pr-[3.75vw] md:pt-0"
+>
+	<div class="flex flex-col gap-2 md:gap-[0.5vw]">
 		<div class="text-3xl font-bold leading-none md:text-[2vw]">
 			Anime <span class="text-warning">Explore</span>
 		</div>
-		<span class="text-base font-normal leading-none md:text-[1.1vw]">
-			Unleash your inner Otaku: Explore anime wonders
-		</span>
+		<span class="text-base font-normal leading-none md:text-[1.1vw]"
+			>Unleash your inner Otaku: Explore anime wonders</span
+		>
 	</div>
-	<div class="mt-5 flex flex-col justify-between text-neutral-300 md:flex-row">
-		<div class="flex flex-col gap-2">
-			<span class="text-base font-semibold leading-none">Search Animes</span>
-			<div class="relative flex items-center">
-				<Search class="pointer-events-none absolute ml-[1vw] w-[1vw]" />
 
-				<input
-					bind:value={search_query}
-					on:input={handle_input}
-					type="text"
-					placeholder="Looking for specific anime? Start from here..."
-					class="text-surface-50 md:bg-surface-400/75 w-[30vw] rounded-[0.5vw] border border-neutral bg-neutral py-[0.8vw] pl-[3vw] text-[1vw] leading-none placeholder:text-neutral-content/75 focus:border-primary focus:ring-0"
-				/>
-			</div>
+	<div class="mt-7 flex flex-col gap-1 md:hidden">
+		<span class="text-base font-semibold leading-none">Search Animes</span>
+		<div class="relative flex items-center">
+			<Search class="pointer-events-none absolute ml-4 w-4" />
+			<input
+				type="text"
+				placeholder="Looking for specific anime? Start from here..."
+				class="w-full rounded-lg border-none bg-neutral py-3 pl-12 leading-none focus:ring-0 md:bg-neutral"
+			/>
 		</div>
+	</div>
 
-		<div class="mt-3 flex items-end gap-2 md:mt-0">
+	<div class="mt-2 flex items-end justify-between gap-3 md:mt-0 md:gap-0">
+		<div class="flex items-center gap-3 md:gap-[1.5vw]">
+			<div class="hidden flex-col gap-[0.35vw] md:flex">
+				<span class="text-[1vw] font-semibold leading-none">Search Animes</span>
+				<div class="relative flex items-center">
+					<div class="absolute md:ml-[1vw]">
+						<Search class="md:w-[1.1vw]" />
+					</div>
+					<input
+						bind:value={search_query}
+						on:input={handle_input}
+						type="text"
+						placeholder="Looking for specific anime? Start from here..."
+						class="w-[30vw] rounded-[0.5vw] border-none bg-neutral py-[0.8vw] pl-[3vw] text-[1vw] font-semibold leading-none text-neutral-content placeholder:font-medium placeholder:text-neutral-content/75 focus:ring-0 md:bg-neutral"
+					/>
+				</div>
+			</div>
 			{#each Object.entries(filter_options_mapping) as option}
 				{@const title = option[1].title}
 				{@const klass = option[1].class}
 				{@const selected_items = option[1].selected_items}
 				{@const filter_items = option[1].items}
 
-				<div class={cn(klass, "group dropdown dropdown-bottom flex-1 md:max-w-36")}>
-					<span class="font-semibold leading-none">{title}</span>
-					<div class="relative mt-1 flex items-center">
+				<div class={cn(klass, "group dropdown dropdown-bottom")}>
+					<span class="font-semibold leading-none md:text-[1vw]">{title}</span>
+					<div class="relative flex items-center">
 						<span class="absolute flex cursor-pointer items-center md:gap-[0.25vw]">
 							{#if selected_items}
-								{#if !isEmpty(selected_items)}
+								{#if !_.isEmpty(selected_items)}
 									<span
-										class="badge badge-primary ml-3 rounded p-1 text-[0.630rem] font-semibold capitalize text-white md:ml-[0.75vw] md:h-[1.5vw] md:rounded-[0.25vw] md:p-[0.35vw]"
+										class="badge badge-primary ml-3 rounded p-1 text-sm font-semibold capitalize md:ml-[0.75vw] md:h-[1.5vw] md:rounded-[0.25vw] md:p-[0.35vw] md:text-[0.85vw]"
 									>
+										<!-- show first item -->
 										{selected_items[0]}
 									</span>
 								{:else}
 									<span
-										class="pointer-events-none ml-3 select-none text-sm duration-300 group-focus-within:opacity-0"
+										class="ml-3 text-base duration-300 group-focus-within:opacity-0 md:ml-[1vw] md:text-[0.9vw]"
 										>Any</span
 									>
 								{/if}
-
+								<!-- show count of remaining items if exists -->
 								{#if selected_items.length > 1}
 									<span
 										class="badge ml-1 rounded p-1 text-sm font-semibold md:ml-[0.15vw] md:h-[1.5vw] md:rounded-[0.25vw] md:p-[0.35vw] md:text-[0.85vw]"
@@ -170,28 +254,30 @@
 							type="text"
 							tabindex="0"
 							role="button"
-							class="peer placeholder w-full rounded-lg border border-neutral bg-neutral py-2 text-sm font-thin leading-none placeholder:font-medium placeholder:text-neutral-content/75 focus-visible:border-[#7569e1] md:max-w-md"
+							class="peer placeholder w-full rounded-lg border-none bg-neutral py-3 text-base font-semibold leading-none text-neutral-content placeholder:font-medium focus:ring-0 md:w-[11vw] md:rounded-[0.5vw] md:bg-neutral md:py-[0.8vw] md:pl-[1vw] md:text-[1vw]"
 						/>
 						{#if selected_items}
-							{#if !isEmpty(selected_items)}
+							{#if !_.isEmpty(selected_items)}
 								<button
 									on:click|preventDefault={() => clear_selected_items(option[0])}
 									class="btn absolute right-0 mr-3 w-4 border-none !bg-transparent p-0 md:mr-[1vw] md:w-[1vw]"
 								>
 									<Cross class="md:w-[1vw]" />
 								</button>
-							{:else}
-								<button
-									class="btn absolute right-0 mr-3 w-4 border-none !bg-transparent p-0 md:mr-[1vw] md:w-[1vw]"
-								>
-									<Chevron class="md:w-[1vw]" />
-								</button>
 							{/if}
+						{:else}
+							<button
+								class="btn absolute right-0 mr-3 w-4 border-none !bg-transparent p-0 md:mr-[1vw] md:w-[1vw]"
+							>
+								<Chevron class="md:w-[1vw]" />
+							</button>
 						{/if}
 					</div>
 
 					{#if filter_items}
-						<div class="dropdown-content z-10 mt-2 w-full overflow-x-hidden rounded-lg">
+						<div
+							class="dropdown-content z-10 mt-2 w-full overflow-x-hidden rounded-lg md:mt-[1vw] md:rounded-[0.5vw]"
+						>
 							<ScrollArea
 								gradient_mask={false}
 								class="flex w-full flex-col md:p-[0.35vw]"
@@ -205,7 +291,7 @@
 											const val = String(value);
 											update_selected_items(option[0], val);
 										}}
-										class="btn btn-neutral relative flex h-max min-h-max items-center justify-start rounded-lg border-2 p-3 py-3 text-sm leading-none focus-visible:border-[#7569e1] focus-visible:bg-secondary/40"
+										class="btn btn-neutral relative flex h-max min-h-max items-center justify-start rounded-none p-3 py-3 text-sm leading-none md:rounded-[0.35vw] md:px-[1vw] md:py-[0.75vw] md:text-[0.9vw]"
 									>
 										<span class="capitalize">{value}</span>
 
@@ -223,18 +309,154 @@
 					{/if}
 				</div>
 			{/each}
-			<button class="btn btn-neutral h-9 min-h-0 w-9 rounded-lg !p-0 md:block">
-				<MoreBox class="w-3" />
-			</button>
 		</div>
+
+		<button
+			class="btn btn-neutral h-max min-h-max rounded-lg p-[0.85rem] md:rounded-[0.5vw] md:p-[0.8vw]"
+		>
+			<MoreBox class="w-4 md:w-[1.1vw]" />
+		</button>
 	</div>
 
-	<div class="mt-8 flex flex-col">
-		<div class="flex flex-col gap-2 md:gap-[0.35vw]">
-			<span class="text-xl font-semibold leading-none text-accent md:text-[1.35vw]"
-				>Trending Now</span
-			>
-			<span class="text-sm leading-none md:text-[1vw]">Crowd Favorites: Anime Hits and Hype</span>
+	<div class="mt-16 md:mt-[1.5vw]">
+		<div class="flex items-center justify-between">
+			<div class="flex flex-col gap-2 md:gap-[0.35vw]">
+				<span class="text-xl font-semibold leading-none text-accent md:text-[1.35vw]"
+					>Trending Now</span
+				>
+				<span class="text-sm leading-none md:text-[1vw]">Crowd Favorites: Anime Hits and Hype</span>
+			</div>
+			<div class="flex gap-3 md:gap-[1vw]">
+				<button class="btn h-max min-h-max border-none !bg-transparent p-0">
+					<Expand class="w-5 md:w-[1.25vw]" />
+					<span class="font-semibold md:text-[1vw]">Trending</span>
+				</button>
+				<div class="divider divider-horizontal m-0"></div>
+				<button
+					class="btn h-max min-h-max border-none !bg-transparent p-0"
+					on:click={() => change_thumbnail_mode("card_with_dropdown")}
+				>
+					<SixGrids class="w-5 md:w-[1.15vw]" />
+				</button>
+				<button
+					class="btn h-max min-h-max border-none !bg-transparent p-0"
+					on:click={() => change_thumbnail_mode("detailed_card")}
+				>
+					<MoreBox class="w-[1.1rem] md:w-[1vw]" />
+				</button>
+			</div>
 		</div>
+
+		{#if search_promise}
+			{#await search_promise}
+				<div class="grid h-full place-items-center">
+					<span class="loading loading-ring loading-lg"></span>
+				</div>
+			{:then results}
+				{#if !_.isEmpty(results)}
+					{#if thumbnail_mode === "detailed_card"}
+						<div class="mt-5 grid grid-cols-2 gap-3 md:mt-[1.25vw] md:grid-cols-3 md:gap-[1.5vw]">
+							{#each results as anime}
+								<a
+									href="/mal/{anime.mal_id}"
+									class="relative col-span-1 grid grid-cols-1 md:grid-cols-2"
+								>
+									<div class="relative">
+										<img
+											src={anime.cover}
+											alt={anime.name}
+											class="h-44 w-full rounded-t-lg object-cover object-center md:h-[20vw] md:rounded-l-[0.35vw] md:rounded-r-none"
+										/>
+										<div
+											class="absolute inset-x-0 bottom-0 rounded-b-lg backdrop-blur md:rounded-l-[0.35vw]"
+										>
+											<div
+												class="flex w-full flex-col bg-secondary/95 p-3 md:gap-[0.35vw] md:p-[1vw]"
+											>
+												<HoverExpand
+													class="w-full text-sm font-semibold text-accent md:text-[1vw] md:leading-[1.35vw]"
+													height="md:max-h-[1.35vw] md:hover:max-h-[10vw]"
+												>
+													{anime.name}
+												</HoverExpand>
+												<span class="line-clamp-1 text-xs md:line-clamp-none md:text-[0.8vw]">
+													{#if anime.studios?.length}
+														{anime.studios[0]}
+													{/if}
+												</span>
+											</div>
+										</div>
+									</div>
+
+									<div
+										class="rounded-rt-none flex flex-col justify-between rounded-b-lg bg-neutral/25 md:rounded-l-none md:rounded-r-[0.35vw]"
+									>
+										<div class="flex flex-col gap-1 p-3 leading-none md:gap-[0.5vw] md:p-[1vw]">
+											<span class="text-xs font-semibold capitalize md:text-[1vw]">
+												{new FormatDate(anime.aired_from ?? "").format_to_season}
+											</span>
+											<div class="flex items-center gap-1 md:gap-[0.5vw]">
+												<span class="text-xs md:text-[0.8vw]">{anime.source}</span>
+												<Circle class="w-1 opacity-50 md:w-[0.25vw]" />
+												<span class="text-xs md:text-[0.8vw]">{anime.episode_count} eps</span>
+											</div>
+											<ScrollArea
+												offset_scrollbar
+												gradient_mask
+												parent_class="max-h-12 md:max-h-[11vw] md:mt-[0.5vw]"
+												class="text-surface-300 text-xs leading-snug md:text-justify md:text-[0.85vw] md:leading-[1vw]"
+											>
+												{anime.synopsis}
+											</ScrollArea>
+										</div>
+
+										<div
+											class="flex items-center gap-2 overflow-x-scroll p-3 pt-0 scrollbar-none md:gap-[0.5vw] md:p-[1vw]"
+										>
+											{#each anime?.genres ?? [] as genre}
+												<span
+													class="whitespace-nowrap rounded-sm bg-warning p-1 text-xs font-semibold capitalize leading-none text-black md:rounded-[0.25vw] md:px-[0.6vw] md:py-[0.3vw] md:text-[0.8vw]"
+												>
+													{genre.name}
+												</span>
+											{/each}
+										</div>
+									</div>
+								</a>
+							{/each}
+						</div>
+					{:else if thumbnail_mode === "card_with_dropdown"}
+						<div class="mt-5 grid grid-cols-3 gap-3 md:mt-[1.25vw] md:grid-cols-6 md:gap-[1.5vw]">
+							<!-- {#each results as item}
+								<AnimeCard
+									anime_mal_id={item?.mal_id ?? 0}
+									anime_name={item.name ?? "N/A"}
+									anime_studios={item.studios ?? []}
+									anime_image={item.cover ?? ""}
+									anime_genres={item.genres ?? []}
+									anime_synopsis={item.synopsis ?? ""}
+									anime_total_episodes={item.episode_count ?? null}
+									anime_rating={item.rating ?? "N/A"}
+								/>
+							{/each} -->
+						</div>
+					{/if}
+				{:else}
+					<div class="flex h-full flex-col items-center justify-center gap-[0.5vw] text-[1.1vw]">
+						<span class="font-medium leading-none">No match found!</span>
+						<span class="text-center font-semibold leading-none text-error">
+							Couldn't find animes with: "{search_query}"
+							<br />
+							<span class="text-accent/75">Filters: {active_filters.join(" - ")}</span>
+						</span>
+					</div>
+				{/if}
+			{:catch error}
+				<div class="flex h-full flex-col items-center justify-center gap-[0.2vw] text-[1.1vw]">
+					<span class="font-medium leading-none">Oh no, something is wrong!</span>
+					<span class="text-center font-semibold leading-none text-error">{@html error}</span>
+				</div>
+			{/await}
+		{/if}
 	</div>
 </section>
