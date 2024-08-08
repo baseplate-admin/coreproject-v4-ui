0de6576c07cef051e7e07e26284ef4ab2bacb7f4
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-08 15:12:42 +0600

    E

--- src/lib/icons/shapes/expand.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import type { SVGParams } from "$types/svg";
-
-	let { class: klass, style, width, height }: SVGParams = $props();
-</script>
-
-<svg class={klass} {style} {width} {height} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-	<path fill="currentColor" d="m18 9l-6-6l-6 6h12Zm0 6l-6 6l-6-6h12Z" />
-</svg>
