8af0930c18851eb3a4dc7da558030479a05d9f7f
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-09 11:32:49 +0600

    Update hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,13 @@
-import type { Handle } from "@sveltejs/kit";
-import { renderToString } from "@coreproject-moe/icons/hydrate";
+// import type { Handle } from "@sveltejs/kit";
+// import { renderToString } from "@coreproject-moe/icons/hydrate";
 
-export const handle = (async ({ event, resolve }) => {
-	const response = await resolve(event, {
-		transformPageChunk: async ({ html }) => {
-			return renderToString(html, {
-				fullDocument: true
-			}).then((results) => results.html ?? "");
-		}
-	});
-	return response;
-}) satisfies Handle;
+// export const handle = (async ({ event, resolve }) => {
+// 	const response = await resolve(event, {
+// 		transformPageChunk: async ({ html }) => {
+// 			return renderToString(html, {
+// 				fullDocument: true
+// 			}).then((results) => results.html ?? "");
+// 		}
+// 	});
+// 	return response;
+// }) satisfies Handle;
